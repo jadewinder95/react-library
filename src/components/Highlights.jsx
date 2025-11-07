615cb4a285c01e5c7fb0e84dbd5fea4ea385b9fb
jadewinder95 <jadewinder95@gmail.com>
2025-11-06 18:58:23 -0800

    look over and tell me why it wont work

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,36 +1,47 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Highlight from "./ui/Highlight";
+import React from 'react'
 
 const Highlights = () => {
-  return (
-    <section id="highlights">
-      <div className="container">
-        <div className="row">
-          <h2 className="section__title">
-            Why choose <span className="purple">Library</span>
-          </h2>
-          <div className="highlight__wrapper">
-            <Highlight
-              icon={<FontAwesomeIcon icon="bolt" />}
-              title="Easy and Quick"
-              para="Get access to the book you purchased online instantly."
-            />
-            <Highlight
-              icon={<FontAwesomeIcon icon="book-open" />}
-              title="10,000+ Books"
-              para="Library has books in all your favourite categories."
-            />
-            <Highlight
-              icon={<FontAwesomeIcon icon="tags" />}
-              title="Affordable"
-              para="Get your hands on popular books for as little as $10."
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="highlights">
+            <div className="container">
+                <div className="row">
+                    <h2 className='section__title'>
+                        why choose <span className='purple'>Library</span>
+                    </h2>
+                    <div className="highlight__wrapper">
+                        <div className="highlight">
+                            <div className="highlight__img">
+                                <FontAwesomeIcon icon='bolt' />
+                            </div>
+                            <h3 className='highlight__subtitle'>Easy and Quick</h3>
+                            <p className='highlight__para'>
+                                Access a vast collection of books and 
+                                resources with just a few clicks.
+                            </p>
+                        </div>
+                        <div className="highlight">
+                            <div className="highlight__img">
+                                <FontAwesomeIcon icon='book-open' />
+                            </div>
+                            <h3 className='highlight__subtitle'>10,000+ Books</h3>
+                            <p className='highlight__para'>
+                                Explore a diverse range of genres and authors.
+                            </p>
+                        </div>
+                        <div className="highlight">
+                            <div className="highlight__img">
+                                <FontAwesomeIcon icon='tags' />
+                            </div>
+                            <h3 className='highlight__subtitle'>Affordable</h3>
+                            <p className='highlight__para'>
+                                Enjoy competitive pricing and great deals.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Highlights;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,67 +1,55 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import LibraryLogo from "../assets/Library.svg";
-import { Link } from "react-router-dom";
-
-const Nav = ({ numberOfItems }) => {
-  function openMenu() {
-    document.body.classList += " menu--open";
-  }
-
-  function closeMenu() {
-    document.body.classList.remove("menu--open");
-  }
-
+import LibraryLogo from '../assets/Library.svg'
+const Nav = () => {
   return (
     <nav>
       <div className="nav__container">
-        <Link to="/">
-          <img className="logo" src={LibraryLogo} alt="" />
-        </Link>
+        <a href="/">
+        <img src={LibraryLogo} alt="" className="logo" />
+        </a>
         <ul className="nav__links">
           <li className="nav__list">
-            <Link to="/" className="nav__link">
+            <a href="" className="nav__link">
               Home
-            </Link>
+            </a>
           </li>
           <li className="nav__list">
-            <Link to="/books" className="nav__link nav__link--primary">
+            <a href="" className="nav__link">
               Books
-            </Link>
+            </a>
           </li>
-          <button className="btn__menu" onClick={openMenu}>
+          <button className="btn__menu">
             <FontAwesomeIcon icon="bars" />
           </button>
           <li className="nav__icon">
-            <Link to="/cart" className="nav__link">
-              <FontAwesomeIcon icon="shopping-cart" />
-            </Link>
-            {numberOfItems > 0 && (
-              <span className="cart__length">{numberOfItems}</span>
-            )}
+            <a href="/cart" className="nav__link">
+            <FontAwesomeIcon icon="shopping-cart" />
+            </a>
+            <span className="cart__length">2</span>
           </li>
         </ul>
-
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+          <button className="btn__menu btn__menu--close">
             <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
             <li className="menu__list">
-              <Link to="/" className="menu__link" onClick={closeMenu}>
-                Home
-              </Link>
+              <a href="/" className="menu__link">
+              Home 
+              </a>
             </li>
             <li className="menu__list">
-              <Link to="/books" className="menu__link" onClick={closeMenu}>
-                Books
-              </Link>
+              <a href="/books" className="menu__link">
+              Books
+              </a>
             </li>
             <li className="menu__list">
-              <Link to="/cart" className="menu__link" onClick={closeMenu}>
-                Cart
-              </Link>
+              <a href="/cart" className="menu__link">
+              Cart
+              </a>
             </li>
+
           </ul>
         </div>
       </div>
